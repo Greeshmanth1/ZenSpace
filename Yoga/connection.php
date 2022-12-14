5c3088263310b7aba4938b92f2f2a683f0573704
Greeshmanth Penugonda <79304704+Greeshmanth1@users.noreply.github.com>
2022-12-14 22:30:13 +0530

    Delete Yoga directory

--- Yoga/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con = mysqli_connect("sql6.freesqldatabase.com","sql6584364","qYdHeQH4b1","sql6584364");
-
-// Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  exit();
-}
-?>
